dfed335a66c648351208437d2f67fcea9e41ef83
Heloisa Tanaka <helo.l.tanaka@gmail.com>
2025-05-11 16:44:08 -0300

    ajustes exercise2

--- 2804/exercise2.py
+++ 2804/exercise2.py
@@ -19,35 +19,74 @@ class Aluno:
         print('Media: ', self.media)
         print('Matrícula: ', self.matricula)
     
-    def troca_turma(self, nova_turma):
-        self.nova_turma = nova_turma
-        self.turma = self.nova_turma
-        return print('Turma: ', self.turma)
+    def setTurma(self, nova_turma):
+        self.turma = nova_turma
     
-    def atualizacao_media(self, nova_media):
-        self.nova_media = nova_media
-        self.media = self.nova_media
-        return print('Média: ', self.media)
+    def setMedia(self, nova_media):
+        self.media = nova_media
+    
+    def setSerie(self, nova_serie):
+        self.serie = nova_serie
+    
+    def getNome(self):
+        return self.nome_completo
+    
+    def getSobrenome(self):
+        return self.sobrenome
+    
+    def getTurma(self):
+        return self.turma
+    
+    def getN_matricula(self):
+        return self.matricula
+    
+    def getMedia(self):
+        return self.media
+    
+    def getSerie(self):
+        return self.serie
 
 class Curso:
-    def __init__(self, curso, duracao, professor, horario, dia):
-        self.curso = curso
+    def __init__(self, nome, duracao, professor, horario, dia):
+        self.nome = nome
         self.duracao = duracao
         self.professor = professor
         self.horario = horario
         self.dia = dia
     
     def infos_curso(self):
-        print('Curso: ', self.curso)
+        print('Curso: ', self.nome)
         print('Duração: ', self.duracao)
         print('Professor: ', self.professor)
         print('Dias: ', self.dia)
         print('Horário: ', self.horario)
 
-    def troca_professor(self, novo_prof):
-        self.novo_prof = novo_prof
+    def setProfessor(self, novo_prof):
         self.professor = novo_prof
-        return print('Professor:', self.professor)
+
+    def setProfessor(self, novo_duracao):
+        self.duracao = novo_duracao
+
+    def setProfessor(self, novo_dia):
+        self.dia = novo_dia
+
+    def setProfessor(self, novo_horario):
+        self.horario = novo_horario
+
+    def getNome(self):
+        return self.nome
+    
+    def getDuracao(self):
+        return self.duracao
+    
+    def getProfessor(self):
+        return self.professor
+    
+    def getHorario(self):
+        return self.horario
+    
+    def getDia(self):
+        return self.dia
 
 class Turma:
     def __init__(self, n_turma, alunos, cursos):
@@ -61,11 +100,10 @@ class Turma:
         print('Cursos: ', self.cursos)
 
     def localizar_aluno(self, aluno_procurado):
-        self.procurado = aluno_procurado
         for x in range(len(list(self.alunos))):
-            if self.alunos[x] == self.procurado:
-                return print (self.procurado, ' pertence à turma ', self.n_turma)           
-        print(self.procurado, ' não pertence à turma ', self.n_turma)
+            if self.alunos[x] == aluno_procurado:
+                return print (aluno_procurado, ' pertence à turma ', self.n_turma)           
+        print(aluno_procurado, ' não pertence à turma ', self.n_turma)
 
     def adicionar_alunos(self, novo_aluno):
         self.novo_aluno = novo_aluno
@@ -85,14 +123,15 @@ class Turma:
         self.cursos.append(novo_curso)
         print('Cursos: ', self.cursos)
 
-Aluno1 = Aluno('Mateus', 16, 610, 23985432, 7.9) 
+Aluno1 = Aluno('Mateus Miranda', 610, 23985432, 7.9, 2) 
 Aluno1.infos_aluno()
-Aluno1.troca_turma(614)
-Aluno1.atualizacao_media(9.2)
+Aluno1.setTurma(614)
+Aluno1.setMedia(9.2)
+Aluno1.infos_aluno()
 
 Artes1 = Curso('Artes1', '70 horas', 'Nakashato', '13:15 - 14:50', 'Segunda-Feira')
 Artes1.infos_curso()
-Artes1.troca_professor('Claudete')
+Artes1.setProfessor('Claudete')
 
 Turma1 = Turma(610, ['Ana Júlia', 'Agatha', 'Roberto', 'Felipe'], ['Artes', 'História', 'Geografia'])
 Turma1.info_turma()
