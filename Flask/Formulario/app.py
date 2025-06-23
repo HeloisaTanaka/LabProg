144ee07ca2e403b93cf2d9496f4f1d280c6f803a
HeloisaTanaka <helo.l.tanaka@gmail.com>
2025-06-23 17:05:28 -0300

    Login

--- Flask/Formulario/app.py
+++ Flask/Formulario/app.py
@@ -3,24 +3,40 @@ from flask import Flask, render_template, session, request, url_for, redirect
 app = Flask(__name__)
 app.secret_key = 'abcd'
 
-
-
 @app.route('/logar', methods=['POST'])
 def login():
-    users = {'marcelinho': 123, 'renata': 456}
+    users = ['marcelinho', 'renata', 'alexandre']
+    passwords = [123, 456, 789]
     username = request.form['username']
     password = request.form['password']
     
-    for i in users:
-        if username == i and password == users.get(i):
+    #verficação de senha n funciona
+    for i in range (len(users)):
+        if username == users[i] and password == passwords[i]:
             session['username'] = username
-            return render_template('profile.html')
+            logado = True
+            produtos = ["Maça", "Banana", "Laranja"]
+            return render_template('home.html', produtos = produtos, username = username, logado = logado)
 
+    logado = False
+    return render_template('profile.html', user = username, logado = logado)
+
+#@app.route('/')
+
+#def carregar():
+#    return render_template('login.html')
+
+@app.route('/user/<username>')
+
+def profile(username):
+    return render_template('profile.html', user=username)
 
 @app.route('/')
-
-def dados():
-    return render_template('login.html')
+def carregarLogin():
+    #produtos = ["Maça", "Banana", "Laranja"]
+    #logado = True
+    #return render_template("login.html", produtos=produtos, logado=logado)
+    return render_template("login.html")
 
 if __name__=='__main__':
     app.run(debug=True)
