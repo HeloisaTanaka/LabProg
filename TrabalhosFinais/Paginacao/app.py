fc098842cbbbad9038912572713c42ed5d670b22
Heloisa Tanaka <helo.l.tanaka@gmail.com>
2025-11-20 15:01:47 -0300

    errorhandler 401, 403, 404, 500

--- TrabalhosFinais/Paginacao/app.py
+++ TrabalhosFinais/Paginacao/app.py
@@ -26,6 +26,21 @@ app.register_blueprint(produtos_bp)
 def index():
     return render_template('index.html')
 
+@app.errorhandler(401)
+def nao_autorizado(error):
+    return render_template('errors/401.html'), 401
+
+@app.errorhandler(403)
+def acesso_proibido(error):
+    return render_template('errors/403.html'), 403
+
+@app.errorhandler(404)
+def pagina_nao_encontrada(error):
+    return render_template('errors/404.html'), 404
+
+@app.errorhandler(500)
+def erro_interno_servidor(error):
+    return render_template('errors/500.html'), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
